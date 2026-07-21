b77aec8d7e1154cb1edeb8f3f6f7b7a3b93b9475
deabow <diabm4930@gmail.com>
2026-07-22 00:39:34 +0300

    Add Meta Pixel tracking script to app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,6 +35,33 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             `
           }}
         />
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;
+              s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1019650937138795');
+              fbq('track', 'PageView');
+            `
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1019650937138795&ev=PageView&noscript=1"
+            alt="Meta Pixel"
+          />
+        </noscript>
         <AppShell>{children}</AppShell>
       </body>
     </html>
