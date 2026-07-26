32128dbed60759c033cccddcaed989c6d9d48d77
deabow <diabm4930@gmail.com>
2026-07-26 17:17:38 +0300

    editس

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,6 +35,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             `
           }}
         />
+        <AppShell>{children}</AppShell>
         <Script
           id="meta-pixel"
           strategy="afterInteractive"
@@ -44,8 +45,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
               n.callMethod.apply(n,arguments):n.queue.push(arguments)};
               if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;
-              s=b.getElementsByTagName(e)[0];
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
               fbq('init', '1019650937138795');
@@ -62,7 +63,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             alt="Meta Pixel"
           />
         </noscript>
-        <AppShell>{children}</AppShell>
       </body>
     </html>
   );
